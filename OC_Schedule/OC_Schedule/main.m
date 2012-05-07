9473bf90a9142e60bac773578a47624a27c15f1a
Kenth Haneling <kenthhaneling@yahoo.se>
2012-05-07 16:04:43 +0200

    Room for lecture changed

--- OC_Schedule/OC_Schedule/main.m
+++ OC_Schedule/OC_Schedule/main.m
@@ -23,7 +23,7 @@ int main(int argc, const char * argv[])
         Course *courseApputv = [Course courseWithCourseId:@"AppUtv2011" coursename:@"Apputveckling för mobila enheter" coursedescription:@"Kursen går ut på att lära sig utveckla appar för mobila enheter som iPad, iPhone och Andriod enehter" coursepoints:400 courseteacher:@"Anders Carlsson" courseLitterature:litterature];
         
         CourseEvent *lecture1 = [CourseEvent courseEventWithDate:[NSDate date] eventStartTime:9 eventStopTime:12 classRoom:@"401" alternetiveTeacher:@"" eventReadingInstructions:@"Läs kap 1" course:courseApputv];
-        CourseEvent *lecture2 = [CourseEvent courseEventWithDate:[NSDate date] eventStartTime:13 eventStopTime:15 classRoom:@"402" alternetiveTeacher:@"" eventReadingInstructions:@"Läs kap 2" course:courseApputv];
+        CourseEvent *lecture2 = [CourseEvent courseEventWithDate:[NSDate date] eventStartTime:13 eventStopTime:15 classRoom:@"403" alternetiveTeacher:@"" eventReadingInstructions:@"Läs kap 2" course:courseApputv];
         
         
         
